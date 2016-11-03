1ee14475e7752fd7a7da17ac5ee2b757ab8dab8b
Chapere <Seii2356:01/->
2016-11-03 09:41:58 +0100

    [timetableErweiterung] -Habe Arbeit an Klassen timetable.scala.html, ClassScheduleController.scala und routes angefangen

--- app/controllers/ClassScheduleController.scala
+++ app/controllers/ClassScheduleController.scala
@@ -1,8 +1,23 @@
 package controllers
 
+import javax.inject._
+import play.api._
+import play.api.mvc._
+
 /**
   * Created by Taha Obed on 27.10.2016.
   */
-class ClassScheduleController {
-
+@Singleton
+class ClassScheduleController @Inject() extends Controller {
+  def timetable = Action {
+    val schedule =
+      List(
+        List("Montag", " Dienstag", " Mittwoch", " Donnerstag", " Freitag"),
+        List("Montag", " Dienstag", " Mittwoch", " Donnerstag", " Freitag"),
+        List("Montag", " Dienstag", " Mittwoch", " Donnerstag", " Freitag"),
+        List("Montag", " Dienstag", " Mittwoch", " Donnerstag", " Freitag"),
+        List("Montag", " Dienstag", " Mittwoch", " Donnerstag", " Freitag")
+      )
+    Ok(views.html.timetable("3.Semester")(schedule))
+  }
 }
